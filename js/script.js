60ed21f5c5bb99e63d6f5536d2fe43211077d910
Pabel <gtxbiscuit@gmail.com>
2022-07-20 02:54:29 +0600

    pic changes

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ var typed = new Typed(".typing",{
     typeSpeed: 80,
     backSpeed: 80,
 });
+
+var typed = new Typed(".typing-2",{
+    strings: ["Web Designer", "Web Developer","Video Editor"],
+    typeSpeed: 100,
+    backSpeed: 60,
+});
